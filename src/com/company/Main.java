200415da930a024ff702e889c8d13e4b5eaa8fcd
rbharrison1 <rbharrison1@crimson.ua.edu>
2017-01-24 22:14:32 -0600

    har har har

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
         Console c = System.console();
-        String input;
+        Date today = new Date();
+        int input;
 
         System.out.print("Enter your age: ");
-        input = c.readLine();
+        input = Integer.parseInt(c.readLine());
 
-        int lowerMonth = Calendar.MONTH;
-        int upperMonth = Calendar.MONTH;
-        int lowerDay = Calendar.DAY_OF_MONTH + 1;
-        int upperDay = Calendar.DAY_OF_MONTH;
-        int lowerYear = Calendar.YEAR - Integer.parseInt(input) - 1;
-        int upperYear = Calendar.YEAR - Integer.parseInt(input);
+        Calendar lower = Calendar.getInstance();
+        lower.setTime(today);
+        Calendar upper = Calendar.getInstance();
+        upper.setTime(today);
 
-        System.out.printf("You were born between the dates of " + lowerMonth + " " + lowerDay + ", " + lowerYear + " and " + upperMonth + " " + upperDay + ", " + upperYear + ".");
+        lower.add(Calendar.YEAR,-input - 1);
+        upper.add(Calendar.YEAR, -input);
+        lower.add(Calendar.DAY_OF_MONTH, 1);
+
+        Date lowerBound = lower.getTime();
+        Date upperBound = upper.getTime();
+
+        System.out.printf("You were born between the dates of " + lowerBound.toString() + " and " + upperBound.toString() + ".");
     }
 }
