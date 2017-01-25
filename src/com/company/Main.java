18e4858f35daa735694183f617943ff4cf18513d
rbharrison1 <rbharrison1@crimson.ua.edu>
2017-01-24 21:03:08 -0600

    date stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,25 @@
 package com.company;
 
-//import java.util.Date;
+import java.util.Calendar;
+import java.util.Date;
 import java.io.Console;
 
 public class Main {
 
     public static void main(String[] args) {
         Console c = System.console();
-        String input = " ";
-        //Date today = new Date();
+        String input;
 
         System.out.print("Enter your age: ");
         input = c.readLine();
 
-        c.printf(input);
+        int lowerMonth = Calendar.MONTH;
+        int upperMonth = Calendar.MONTH;
+        int lowerDay = Calendar.DAY_OF_MONTH + 1;
+        int upperDay = Calendar.DAY_OF_MONTH;
+        int lowerYear = Calendar.YEAR - Integer.parseInt(input) - 1;
+        int upperYear = Calendar.YEAR - Integer.parseInt(input);
+
+        System.out.printf("You were born between the dates of " + lowerMonth + " " + lowerDay + ", " + lowerYear + " and " + upperMonth + " " + upperDay + ", " + upperYear + ".");
     }
 }
